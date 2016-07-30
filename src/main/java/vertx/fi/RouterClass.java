19b3cd4db75aff658468082dfa9629bd4c6c3852
yangmyfly <1368035409@qq.com>
2016-07-30 00:36:16 -0400

    distance

--- src/main/java/vertx/fi/RouterClass.java
+++ src/main/java/vertx/fi/RouterClass.java
@@ -686,11 +686,11 @@ public class RouterClass {
 
         double ratio = rssi*1.0/(txPower-41);
         if (ratio < 1.0) {
-            return Math.pow(ratio,10) * 100;
+            return Math.pow(ratio,10) * 10;
         }
         else {
             double accuracy =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;
-            return accuracy * 100;
+            return accuracy * 10;
         }
     }
 
